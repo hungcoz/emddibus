bdfff755cce708cdd4e0bd602dc12e5caf887e69
HaAnhHung <hahunguetk62@gmail.com>
2021-03-06 13:09:20 +0700

    fix FloatingActionButton to get currentPosition

--- lib/pages/BusPath/bus_path_screen.dart
+++ lib/pages/BusPath/bus_path_screen.dart
@@ -90,90 +90,96 @@ class ShowBusPathState extends State<ShowBusPath> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: Text("Tuyến " + routeId.toString()),
       ),
-      body: Stack(
-        children: [
-          FlutterMap(
-            mapController: mapController,
-            options: MapOptions(
-              maxZoom: 18,
-              center: LatLng(_listPointGo[0].latitude, _listPointGo[0].longitude),
-              zoom: 5,
-              plugins: [
-                LocationPlugin(),
-              ],
-              interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate,
-            ),
-            layers: [
-              TileLayerOptions(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      body: SafeArea(
+        child: Stack(
+          children: [
+            FlutterMap(
+              mapController: mapController,
+              options: MapOptions(
+                maxZoom: 18,
+                center: LatLng(_listPointGo[0].latitude, _listPointGo[0].longitude),
+                onTap: (_){},
+                zoom: 16,
+                plugins: [
+                  LocationPlugin(),
+                ],
+                interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate,
               ),
-              MarkerLayerOptions(markers: markers),
-              LocationOptions(
-                  markers: markers,
-                  onLocationUpdate: (LatLngData ld) {
-                    setState(() {
-                      currentPosition = ld.location;
-                    });
-                  },
-                  onLocationRequested: (LatLngData ld) {
-                    if (ld == null || ld.location == null) {
-                      return;
-                    }
-                    mapController?.move(ld.location, 16);
-                  },
-                  buttonBuilder: (BuildContext context,
-                      ValueNotifier<LocationServiceStatus> status,
-                      Function onPressed) {
-                    return Positioned(
-                      bottom: 20,
-                      right: 20,
-                      child: FloatingActionButton(
-                        child: ValueListenableBuilder<LocationServiceStatus>(
-                          valueListenable: status,
-                          builder:
-                              (context, LocationServiceStatus value, child) {
-                            switch (value) {
-                              case LocationServiceStatus.disabled:
-                              case LocationServiceStatus.permissionDenied:
-                              case LocationServiceStatus.unsubscribed:
-                                return Icon(
-                                  Icons.location_disabled,
-                                  color: Colors.black,
-                                );
-                                break;
-                              default:
-                                return Icon(
-                                  Icons.my_location,
-                                  color: Colors.black,
-                                );
-                                break;
-                            }
-                          },
-                        ),
-                        onPressed: () => onPressed(),
-                        backgroundColor: Colors.white,
+              layers: [
+                TileLayerOptions(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                ),
+                MarkerLayerOptions(markers: markers),
+                PolylineLayerOptions(
+                    polylines: [
+                      Polyline(
+                        strokeWidth: 4,
+                        points: _listPointGo,
                       ),
-                    );
-                  }),
-              PolylineLayerOptions(
-                  polylines: [
-                    Polyline(
-                      strokeWidth: 4,
-                      points: _listPointGo,
-                    ),
-                    Polyline(
-                      color: Colors.deepOrangeAccent,
-                      strokeWidth: 4,
-                      points: _listPointReturn,
-                    )
-                  ]
-              )
-            ],
-          ),
-        ],
+                      Polyline(
+                        color: Colors.deepOrangeAccent,
+                        strokeWidth: 4,
+                        points: _listPointReturn,
+                      )
+                    ]
+                ),
+                LocationOptions(
+                    markers: markers,
+                    onLocationUpdate: (LatLngData ld) {
+                      setState(() {
+                        currentPosition = ld.location;
+                      });
+                    },
+                    onLocationRequested: (LatLngData ld) {
+                      if (ld == null || ld.location == null) {
+                        return;
+                      }
+                      // mapController?.move(ld.location, 16);
+                    },
+                    buttonBuilder: (BuildContext context,
+                        ValueNotifier<LocationServiceStatus> status,
+                        Function onPressed) {
+                      return Positioned(
+                        bottom: 20,
+                        right: 20,
+                        child: FloatingActionButton(
+                          child: ValueListenableBuilder<LocationServiceStatus>(
+                            valueListenable: status,
+                            builder:
+                                (context, LocationServiceStatus value, child) {
+                              switch (value) {
+                                case LocationServiceStatus.disabled:
+                                case LocationServiceStatus.permissionDenied:
+                                case LocationServiceStatus.unsubscribed:
+                                  return Icon(
+                                    Icons.location_disabled,
+                                    color: Colors.black,
+                                  );
+                                  break;
+                                default:
+                                  return Icon(
+                                    Icons.my_location,
+                                    color: Colors.black,
+                                  );
+                                  break;
+                              }
+                            },
+                          ),
+                          onPressed:(){
+                            mapController?.move(currentPosition, 16);
+                        },
+                          backgroundColor: Colors.white,
+                        ),
+                      );
+                    }),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
