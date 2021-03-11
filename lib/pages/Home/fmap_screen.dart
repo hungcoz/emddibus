ecca0b62fe33500551cc37856e620b629d4fa90c
hungcoz <17020789@vnu.edu.vn>
2021-03-11 14:41:53 +0700

    fix map

--- lib/pages/Home/fmap_screen.dart
+++ lib/pages/Home/fmap_screen.dart
@@ -1,6 +1,7 @@
 import 'package:emddibus/constants.dart';
 import 'package:emddibus/pages/Home/search_field.dart';
 import 'package:emddibus/pages/Home/stop_point_marker.dart';
+import 'package:emddibus/pages/Map/map.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_location/flutter_map_location.dart';
@@ -51,73 +52,74 @@ class _FMapState extends State<FMap> {
       ),
       body: SafeArea(
         child: Stack(children: [
-          FlutterMap(
-            mapController: mapController,
-            options: MapOptions(
-              maxZoom: 18,
-              minZoom: 5,
-              center: LatLng(15.594016, 110.450604),
-              zoom: 5,
-              onTap: (_) => _textSearchFocusNode.unfocus(),
-              plugins: [
-                LocationPlugin(),
-              ],
-              interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate,
-            ),
-            layers: [
-              TileLayerOptions(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-              ),
-              MarkerLayerOptions(markers: markers),
-              LocationOptions(
-                  markers: markers,
-                  onLocationUpdate: (LatLngData ld) {
-                    setState(() {
-                      currentPosition = ld.location;
-                    });
-                  },
-                  onLocationRequested: (LatLngData ld) {
-                    if (ld == null || ld.location == null) {
-                      return;
-                    }
-                    mapController?.move(ld.location, 16);
-                  },
-                  buttonBuilder: (BuildContext context,
-                      ValueNotifier<LocationServiceStatus> status,
-                      Function onPressed) {
-                    return Positioned(
-                      bottom: 20,
-                      right: 20,
-                      child: FloatingActionButton(
-                        child: ValueListenableBuilder<LocationServiceStatus>(
-                          valueListenable: status,
-                          builder:
-                              (context, LocationServiceStatus value, child) {
-                            switch (value) {
-                              case LocationServiceStatus.disabled:
-                              case LocationServiceStatus.permissionDenied:
-                              case LocationServiceStatus.unsubscribed:
-                                return Icon(
-                                  Icons.location_disabled,
-                                  color: Colors.black,
-                                );
-                                break;
-                              default:
-                                return Icon(
-                                  Icons.my_location,
-                                  color: Colors.black,
-                                );
-                                break;
-                            }
-                          },
-                        ),
-                        onPressed: () => onPressed(),
-                        backgroundColor: Colors.white,
-                      ),
-                    );
-                  }),
-            ],
-          ),
+          Map(mapController: mapController, initialCamera: LatLng(15.594016, 110.450604), initialZoom: 5, markers: markers, focusNode: _textSearchFocusNode),
+          // FlutterMap(
+          //   mapController: mapController,
+          //   options: MapOptions(
+          //     maxZoom: 18,
+          //     minZoom: 5,
+          //     center: LatLng(15.594016, 110.450604),
+          //     zoom: 5,
+          //     onTap: (_) => _textSearchFocusNode.unfocus(),
+          //     plugins: [
+          //       LocationPlugin(),
+          //     ],
+          //     interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate,
+          //   ),
+          //   layers: [
+          //     TileLayerOptions(
+          //       urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          //     ),
+          //     MarkerLayerOptions(markers: markers),
+          //     LocationOptions(
+          //         markers: markers,
+          //         onLocationUpdate: (LatLngData ld) {
+          //           setState(() {
+          //             currentPosition = ld.location;
+          //           });
+          //         },
+          //         onLocationRequested: (LatLngData ld) {
+          //           if (ld == null || ld.location == null) {
+          //             return;
+          //           }
+          //           mapController?.move(ld.location, 16);
+          //         },
+          //         buttonBuilder: (BuildContext context,
+          //             ValueNotifier<LocationServiceStatus> status,
+          //             Function onPressed) {
+          //           return Positioned(
+          //             bottom: 20,
+          //             right: 20,
+          //             child: FloatingActionButton(
+          //               child: ValueListenableBuilder<LocationServiceStatus>(
+          //                 valueListenable: status,
+          //                 builder:
+          //                     (context, LocationServiceStatus value, child) {
+          //                   switch (value) {
+          //                     case LocationServiceStatus.disabled:
+          //                     case LocationServiceStatus.permissionDenied:
+          //                     case LocationServiceStatus.unsubscribed:
+          //                       return Icon(
+          //                         Icons.location_disabled,
+          //                         color: Colors.black,
+          //                       );
+          //                       break;
+          //                     default:
+          //                       return Icon(
+          //                         Icons.my_location,
+          //                         color: Colors.black,
+          //                       );
+          //                       break;
+          //                   }
+          //                 },
+          //               ),
+          //               onPressed: () => onPressed(),
+          //               backgroundColor: Colors.white,
+          //             ),
+          //           );
+          //         }),
+          //   ],
+          // ),
           SearchField(
             txtSearchFocusNode: _textSearchFocusNode,
             mapController: mapController,
